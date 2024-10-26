cb2fa51b066b7390cb41f086438e25b7d36ab739
Prasanna0712 <149136799+Prasanna0712@users.noreply.github.com>
2024-10-26 13:01:12 +0530

    updated login and signup page

--- Login-Signup-Form-main/src/index.js
+++ Login-Signup-Form-main/src/index.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const path = require("path")
 const app = express()
-// const hbs = require("hbs")
-const LogInCollection = require("./mongo")
-const port = process.env.PORT || 3000
+
+ // const LogInCollection = require("./mongo")
+const port = process.env.PORT || 3001
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: false }))
@@ -17,7 +17,7 @@ app.set('views', tempelatePath)
 app.use(express.static(publicPath))
 
 
-// hbs.registerPartials(partialPath)
+
 
 
 app.get('/signup', (req, res) => {
@@ -29,68 +29,62 @@ app.get('/', (req, res) => {
 
 
 
-// app.get('/home', (req, res) => {
-//     res.render('home')
+ 
+
+// app.post('/signup', async (req, res) => {
+    
+    
+
+//     const data = {
+//         name: req.body.name,
+//         password: req.body.password
+//     }
+
+//     const checking = await LogInCollection.findOne({ name: req.body.name })
+
+//    try{
+//     if (checking.name === req.body.name && checking.password===req.body.password) {
+//         res.send("user details already exists")
+//     }
+//     else{
+//         await LogInCollection.insertMany([data])
+//     }
+//    }
+//    catch{
+//     res.send("wrong inputs")
+//    }
+
+//     res.status(201).render("home", {
+//         naming: req.body.name
+//     })
 // })
 
-app.post('/signup', async (req, res) => {
+
+// app.post('/login', async (req, res) => {
+
+//     try {
+//         const check = await LogInCollection.findOne({ name: req.body.name })
+
+//         if (check.password === req.body.password) {
+//             res.status(201).render("home", { naming: `${req.body.password}+${req.body.name}` })
+//         }
+
+//         else {
+//             res.send("incorrect password")
+//         }
+
+
+//     } 
     
-    // const data = new LogInCollection({
-    //     name: req.body.name,
-    //     password: req.body.password
-    // })
-    // await data.save()
+//     catch (e) {
 
-    const data = {
-        name: req.body.name,
-        password: req.body.password
-    }
-
-    const checking = await LogInCollection.findOne({ name: req.body.name })
-
-   try{
-    if (checking.name === req.body.name && checking.password===req.body.password) {
-        res.send("user details already exists")
-    }
-    else{
-        await LogInCollection.insertMany([data])
-    }
-   }
-   catch{
-    res.send("wrong inputs")
-   }
-
-    res.status(201).render("home", {
-        naming: req.body.name
-    })
-})
-
-
-app.post('/login', async (req, res) => {
-
-    try {
-        const check = await LogInCollection.findOne({ name: req.body.name })
-
-        if (check.password === req.body.password) {
-            res.status(201).render("home", { naming: `${req.body.password}+${req.body.name}` })
-        }
-
-        else {
-            res.send("incorrect password")
-        }
-
-
-    } 
-    
-    catch (e) {
-
-        res.send("wrong details")
+//         res.send("wrong details")
         
 
-    }
+//     }
 
 
-})
+// })
 
 
 
--- Login-Signup-Form-main/src/mongo.js
+++ Login-Signup-Form-main/src/mongo.js
@@ -1,24 +1,24 @@
-const mongoose=require("mongoose")
+// const mongoose=require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/LoginFormPractice")
-.then(()=>{
-    console.log('mongoose connected');
-})
-.catch((e)=>{
-    console.log('failed');
-})
+// mongoose.connect("mongodb://localhost:27017/LoginFormPractice")
+// .then(()=>{
+//     console.log('mongoose connected');
+// })
+// .catch((e)=>{
+//     console.log('failed');
+// })
 
-const logInSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    }
-})
+// const logInSchema=new mongoose.Schema({
+//     name:{
+//         type:String,
+//         required:true
+//     },
+//     password:{
+//         type:String,
+//         required:true
+//     }
+// })
 
-const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
+// const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
 
-module.exports=LogInCollection
+// module.exports=LogInCollection
